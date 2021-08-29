9c4c596bfbfe4e32ad1bcd3382bcd67cb400a90d
Gu <936285702@qq.com>
2021-08-29 18:23:32 +0800

    feat(all): 响应数据添加泛型
    响应数据添加泛型

--- examples/extend/app.ts
+++ examples/extend/app.ts
@@ -2,7 +2,7 @@
  * @Author: GZH
  * @Date: 2021-08-28 22:25:41
  * @LastEditors: GZH
- * @LastEditTime: 2021-08-28 22:54:07
+ * @LastEditTime: 2021-08-29 18:16:41
  * @FilePath: \ts-axios\examples\extend\app.ts
  * @Description:
  */
@@ -38,43 +38,43 @@ import axios from '../../src/index'
 
 // axios.patch('/extend/patch', { msg: 'patch' })
 
-axios({
-  url: '/extend/post',
-  method: 'post',
-  data: {
-    msg: 'hi'
-  }
-})
-
-axios('/extend/post', {
-  method: 'post',
-  data: {
-    msg: 'hello'
-  }
-})
-
-// interface ResponseData<T = any> {
-//   code: number
-//   result: T
-//   message: string
-// }
-
-// interface User {
-//   name: string
-//   age: number
-// }
-
-// function getUser<T>() {
-//   return axios<ResponseData<T>>('/extend/user')
-//     .then(res => res.data)
-//     .catch(err => console.error(err))
-// }
-
-// async function test() {
-//   const user = await getUser<User>()
-//   if (user) {
-//     console.log(user.result.name)
+// axios({
+//   url: '/extend/post',
+//   method: 'post',
+//   data: {
+//     msg: 'hi'
 //   }
-// }
+// })
 
-// test()
+// axios('/extend/post', {
+//   method: 'post',
+//   data: {
+//     msg: 'hello'
+//   }
+// })
+
+interface ResponseData<T = any> {
+  code: number
+  result: T
+  message: string
+}
+
+interface User {
+  name: string
+  age: number
+}
+
+function getUser<T>() {
+  return axios<ResponseData<T>>('/extend/user')
+    .then(res => res.data)
+    .catch(err => console.error(err))
+}
+
+async function test() {
+  const user = await getUser<User>()
+  if (user) {
+    console.log(user.result.name)
+  }
+}
+
+test()
--- src/type/index.ts
+++ src/type/index.ts
@@ -2,7 +2,7 @@
  * @Author: GZH
  * @Date: 2021-08-22 10:12:17
  * @LastEditors: GZH
- * @LastEditTime: 2021-08-28 22:48:21
+ * @LastEditTime: 2021-08-29 18:20:30
  * @FilePath: \ts-axios\src\type\index.ts
  * @Description:
  */
@@ -33,8 +33,8 @@ export interface AxiosRequestConfig {
   timeout?: number
 }
 
-export interface AxiosResponse {
-  data: any
+export interface AxiosResponse<T = any> {
+  data: T
   status: number
   statusText: string
   headers: any
@@ -42,7 +42,7 @@ export interface AxiosResponse {
   request: any
 }
 
-export interface AxiosPromise extends Promise<AxiosResponse> {}
+export interface AxiosPromise<T = any> extends Promise<AxiosResponse<T>> {}
 
 export interface AxiosError extends Error {
   isAxiosError: boolean
@@ -53,24 +53,24 @@ export interface AxiosError extends Error {
 }
 
 export interface Axios {
-  request(config: AxiosRequestConfig): AxiosPromise
+  request<T = any>(config: AxiosRequestConfig): AxiosPromise<T>
 
-  get(url: string, config?: AxiosRequestConfig): AxiosPromise
+  get<T = any>(url: string, config?: AxiosRequestConfig): AxiosPromise<T>
 
-  delete(url: string, config?: AxiosRequestConfig): AxiosPromise
+  delete<T = any>(url: string, config?: AxiosRequestConfig): AxiosPromise<T>
 
-  head(url: string, config?: AxiosRequestConfig): AxiosPromise
+  head<T = any>(url: string, config?: AxiosRequestConfig): AxiosPromise<T>
 
-  options(url: string, config?: AxiosRequestConfig): AxiosPromise
+  options<T = any>(url: string, config?: AxiosRequestConfig): AxiosPromise<T>
 
-  post(url: string, data?: any, config?: AxiosRequestConfig): AxiosPromise
+  post<T = any>(url: string, data?: any, config?: AxiosRequestConfig): AxiosPromise<T>
 
-  put(url: string, data?: any, config?: AxiosRequestConfig): AxiosPromise
+  put<T = any>(url: string, data?: any, config?: AxiosRequestConfig): AxiosPromise<T>
 
-  patch(url: string, data?: any, config?: AxiosRequestConfig): AxiosPromise
+  patch<T = any>(url: string, data?: any, config?: AxiosRequestConfig): AxiosPromise<T>
 }
 
 export interface AxiosInstance extends Axios {
-  (config: AxiosRequestConfig): AxiosPromise
-  (url: string, config?: AxiosRequestConfig): AxiosPromise
+  <T = any>(config: AxiosRequestConfig): AxiosPromise<T>
+  <T = any>(url: string, config?: AxiosRequestConfig): AxiosPromise<T>
 }
